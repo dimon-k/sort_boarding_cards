2749a56477fb88c095efa8c80cd26acf43ea917c
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2020-01-09 18:58:27 +0100

    Refactor PrettyResult class

--- app/pretty_result.rb
+++ app/pretty_result.rb
@@ -4,27 +4,37 @@ class PrettyResult
   end
 
   def call
-    result.map do |card|
-      case card[:type]
-      when 'train'
-        "Take #{card[:type]} #{card[:number]} from #{card[:from]} to #{card[:to]}. Sit in seat #{card[:seat]}."
-      when 'airport bus'
-        "Take the #{card[:type]} from #{card[:from]} to #{card[:to]}. No seat assignment."
-      when 'airplane'
-        "From #{card[:from]}, take flight #{card[:number]} to #{card[:to]}. Gate #{card[:gate]}, seat #{card[:seat]}. ".concat(baggage(card, result))
-      end
-    end.append('You have arrived at your final destination.')
+    result.map { |card| build_a_message(card) }
+          .append(final_destination_message)
   end
 
   private
 
   attr_reader :result
 
-  def baggage(card, result)
-    if card.eql?(result.last)
-      "Baggage will we automatically transferred from your last leg."
-    else
-      "Baggage drop at ticket counter #{card[:counter]}."
-    end
+  def build_a_message(card)
+    send("#{card[:type].gsub(' ', '_')}_message", card)
+  end
+
+  def train_message(card)
+    "Take #{card[:type]} #{card[:number]} from #{card[:from]} to #{card[:to]}. Sit in seat #{card[:seat]}."
+  end
+
+  def airport_bus_message(card)
+    "Take the #{card[:type]} from #{card[:from]} to #{card[:to]}. No seat assignment."
+  end
+
+  def airplane_message(card)
+    "From #{card[:from]}, take flight #{card[:number]} to #{card[:to]}. Gate #{card[:gate]}, seat #{card[:seat]}. ".concat(baggage(card, result.last))
+  end
+
+  def baggage(card, last_result)
+    return "Baggage drop at ticket counter #{card[:counter]}." unless card.eql?(last_result)
+
+    "Baggage will we automatically transferred from your last leg."
+  end
+
+  def final_destination_message
+    'You have arrived at your final destination.'
   end
 end
